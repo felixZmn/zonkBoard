34101e67786b20f8f518f8fd67ff2a77d5a8235f
Teddy <felix.zimmermann.1.de@gmail.com>
2019-12-30 14:47:09 +0100

    added sounds

--- app/src/main/java/com/teddy/zonkboard/MainActivity.java
+++ app/src/main/java/com/teddy/zonkboard/MainActivity.java
@@ -1,27 +1,68 @@
 package com.teddy.zonkboard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    static MediaPlayer mp = new MediaPlayer();
+    int playing = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = this.findViewById(R.id.buttonPlay);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.zonk);
+        Button buttonZonk = this.findViewById(R.id.buttonPlayZonk);
+        Button buttonBadum = this.findViewById(R.id.buttonPlayBadum);
+        Button buttonJeopardy = this.findViewById(R.id.buttonPlayJeopardy);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonZonk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                if (mp.isPlaying())
+                    mp.stop();
+                else
+                    playSound(R.raw.zonk);
             }
         });
+
+        buttonBadum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying())
+                    mp.stop();
+                else
+                    playSound(R.raw.badumtss);
+            }
+        });
+
+        buttonJeopardy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying())
+                    mp.stop();
+                else
+                    playSound(R.raw.jeopardy);
+            }
+        });
+    }
+
+    private void playSound(int sound) {
+        mp.reset();
+        try {
+            mp.setDataSource(getApplicationContext(), Uri.parse("android.resource://com.teddy.zonkboard/" + sound));
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
