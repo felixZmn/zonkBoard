f00a1b395bf340ead66324b5d929f4ceeb91fa0d
felixZmn <felix.zimmermann.1.de@gmail.com>
2020-12-12 22:11:48 +0100

    Added Windows shutdown sound

--- app/src/main/java/com/teddy/zonkboard/MainActivity.java
+++ app/src/main/java/com/teddy/zonkboard/MainActivity.java
@@ -1,9 +1,10 @@
 package com.teddy.zonkboard;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,46 +12,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
     static MediaPlayer mp = new MediaPlayer();
-    int playing = 0;
+    int maxVolume;
+    int volume;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mp.setOnCompletionListener(mp -> audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0));
+
         Button buttonZonk = this.findViewById(R.id.buttonPlayZonk);
         Button buttonBadum = this.findViewById(R.id.buttonPlayBadum);
         Button buttonJeopardy = this.findViewById(R.id.buttonPlayJeopardy);
+        Button buttonShutdown = this.findViewById(R.id.buttonPlayShutdown);
 
-        buttonZonk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mp.isPlaying())
-                    mp.stop();
-                else
-                    playSound(R.raw.zonk);
+        buttonZonk.setOnClickListener(v -> {
+            if (mp.isPlaying()) {
+                mp.stop();
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+            } else {
+                playSound(R.raw.zonk);
             }
         });
 
-        buttonBadum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mp.isPlaying())
-                    mp.stop();
-                else
-                    playSound(R.raw.badumtss);
+        buttonBadum.setOnClickListener(v -> {
+            if (mp.isPlaying()) {
+                mp.stop();
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+            } else {
+                playSound(R.raw.badumtss);
             }
         });
 
-        buttonJeopardy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mp.isPlaying())
-                    mp.stop();
-                else
-                    playSound(R.raw.jeopardy);
+        buttonJeopardy.setOnClickListener(v -> {
+            if (mp.isPlaying()) {
+                mp.stop();
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+            } else {
+                playSound(R.raw.jeopardy);
+            }
+        });
+
+        buttonShutdown.setOnClickListener(v -> {
+            if (mp.isPlaying()) {
+                mp.stop();
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+            } else {
+                playSound(R.raw.shutdown);
             }
         });
     }
@@ -58,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
     private void playSound(int sound) {
         mp.reset();
         try {
+            maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
             mp.setDataSource(getApplicationContext(), Uri.parse("android.resource://com.teddy.zonkboard/" + sound));
             mp.prepare();
             mp.start();
